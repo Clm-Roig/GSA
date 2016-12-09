e6eea31f505496e1a7025279df9a2512fc8ebe19
Clément Roig <clm.roig@gmail.com>
2016-12-09 15:42:00 +0100

    Set + get de l'entête de Couleur

--- src/Couleur.h
+++ src/Couleur.h
@@ -7,5 +7,64 @@
 // ---- CONSTANTES ---- //
 
 
+// ---- STRUCTURE ---- //
+/** @struct Couleur
+ *  @brief Structure definissant une couleur au format R-G-B
+ *  @var Couleur::rCoul
+ *  Valeur de Rouge (compris entre 0 et 255)
+ *  @var Couleur::gCoul
+ *  Valeur de Vert (compris entre 0 et 255)
+ *  @var Couleur::bCoul
+ *  Valeur de Bleu (compris entre 0 et 255)
+ */
+typedef struct Couleur {
+    int rCoul;
+    int gCoul
+    int bCoul;
+} Couleur;
+
 // ---- FONCTIONS ---- //
 
+//Getters
+/**
+	@brief Recupère rCoul d'une Couleur
+	@param Couleur* couleur, la couleur à lire
+	@return int rCoul, la valeur de rouge de la Couleur
+*/
+int getRCoul(Couleur* couleur);
+
+/**
+	@brief Recupère gCoul d'une Couleur
+	@param Couleur* couleur, la couleur à lire
+	@return int gCoul, la valeur de rouge de la Couleur
+*/
+int getGCoul(Couleur* couleur);
+
+/**
+	@brief Recupère bCoul d'une Couleur
+	@param Couleur* couleur, la couleur à lire
+	@return int bCoul, la valeur de rouge de la Couleur
+*/
+int getBCoul(Couleur* couleur);
+
+// Setters
+/**
+	@brief Edition de rCoul dans la structure
+	@param int r, la valeur de rouge à setter (compris entre 0 et 255)
+	@param Couleur* couleur, la couleur à modifier
+*/
+void setRCoul(int r,Couleur* couleur);
+
+/**
+	@brief Edition de gCoul dans la structure
+	@param int g, la valeur de vert à setter (compris entre 0 et 255)
+	@param Couleur* couleur, la couleur à modifier
+*/
+void setGCoul(int g,Couleur* couleur);
+
+/**
+	@brief Edition de bCoul dans la structure
+	@param int b, la valeur de bleu à setter (compris entre 0 et 255)
+	@param Couleur* couleur la couleur à modifier
+*/
+void setBCoul(int b,Couleur* couleur);
