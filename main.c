8a4f5a14707f4e9bc3a9114a16f4d93d2b3968dd
Clément Roig <clm.roig@gmail.com>
2016-12-10 17:00:07 +0100

    MaJ includes du main

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 // Programme principal du projet COAL
 #include <stdlib.h>
 #include <stdio.h>
+
 #include "src/BDD.c"
+#include "src/Couleur.c"
+#include "src/ReconnaissanceV.c"
 
 int main() {
     return 0;
