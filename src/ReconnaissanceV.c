0d734d1ab36b1163e1227baf47fc7d7a72a58d82
Clément Roig <clm.roig@gmail.com>
2016-12-10 16:48:11 +0100

    Create ReconnaissanceV + add fichier tomate.jpg

---
+++ src/ReconnaissanceV.c
@@ -0,0 +1,1 @@
+#include "ReconnaissanceV.h"
---
+++ src/ReconnaissanceV.h
@@ -0,0 +1,10 @@
+// EN-TETE Couleur.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+// ---- CONSTANTES ---- //
+
+// ---- FONCTIONS ---- //
+
