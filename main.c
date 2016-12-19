beb87f10a2a735e55bda4a432cba249fc40d64d9
Clément Roig <clm.roig@gmail.com>
2016-12-19 13:51:01 +0100

    Modif main (tests)
    J'ai divisé le main en plusieurs parties pour les tests. Il suffit de commenter/décommenter la partie qui vous concerne pas à chaque fois que vous désirez tester quelque chose :)

--- main.c
+++ main.c
@@ -2,9 +2,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "src/BDD.c"
+// #include "src/BDD.c"
+#include "LecteurBMP.c"
 
 int main() {
-    setDatePesee(5,"09/07/1995");
+    /* ---- TESTS BDD (Matthieu) ---- */
+   /* setDatePesee(5,"09/07/1995");
+
+    /* ---- TESTS LecteurBMP(Clément) ---- */
+
+
     return 0;
 }
