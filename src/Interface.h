f9e1abb66dde906e196fe71f5c4d49bb37f5aa1c
Clément Roig <clm.roig@gmail.com>
2016-12-24 13:56:25 +0100

    Squelettes des fonctions d'Interface

---
+++ src/Interface.h
@@ -0,0 +1,22 @@
+#ifndef DEF_Interface
+#define DEF_Interface
+
+// EN-TETE Interface.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+// ---- CONSTANTES ---- //
+//const char* CHEMIN_IMAGES_ALIMENTS = "data/images_aliments/";
+
+// ---- FONCTIONS ---- //
+
+/**
+	@brief
+	@param
+	@return
+*/
+int foo();
+
+#endif
