a7f680b272f93d2b494ec45f443ee0012d1f0cd8
Clément Roig <clm.roig@gmail.com>
2016-12-31 00:12:25 +0100

    nettoyage du main de test

--- main.c
+++ main.c
@@ -4,13 +4,6 @@
 
 #include "src/BDD.c"
 
-
 int main() {
-    /* ---- TESTS Interface(Clément) ---- */
-    Couleur* coul = initCouleur();
-    setRCoul(3,coul);
-    setGCoul(255,coul);
-    setBCoul(147,coul);
-    getIdAlimentParCouleur(coul,1);
     return 0;
 }
