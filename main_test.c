8652d2b52c21c97a9b827aa4475505b56eb9d8fd
Clément Roig <clm.roig@gmail.com>
2017-01-03 14:28:13 +0100

    main.c => main_test

--- main_test.c
+++ main_test.c
@@ -1,10 +1,10 @@
-// Programme principal du projet COAL
+// Programme de test du projet COAL
 #include <stdlib.h>
 #include <stdio.h>
 
 #include "src/BDD.c"
 
-int main() {
+int main_test() {
     ecrireDonneePesee("","une pesee de 2017","01/01/2017",3);
     return 0;
 }
