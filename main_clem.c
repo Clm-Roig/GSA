026ddf64fe70705af402d4a9a6594815e64bc0d6
Clément Roig <clm.roig@gmail.com>
2017-01-06 18:43:34 +0100

    [RecV] Ajustement du seuil pour repérer un aliment sur un fond

--- main_clem.c
+++ main_clem.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "src/BDD.c"
+#include "src/ReconnaissanceV.c"
 
 int main() {
-    printf("%d",getDureePeremptionAliment(3));
+    Couleur* blanc = initCouleur();
+    setRCoul(255,blanc);
+    setGCoul(255,blanc);
+    setBCoul(255,blanc);
+
+    FILE* fichier = fopen("data/images_aliments/tomate.bmp","rb");
+    ImageBMP* img = initImageBMP(fichier);
+    printf("\nCouleur dominante : %s",toCharCouleur(couleurDominante(img)));
+    printf("\nCouleur dominante hors fond : %s",toCharCouleur(couleurDominanteHorsFond(img,blanc)));
+
+    fclose(fichier);
     return 0;
 }
