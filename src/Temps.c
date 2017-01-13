cbcf07802fb3424b45ef0d45ca8808724f5b62fc
Clément Roig <clm.roig@gmail.com>
2017-01-13 17:22:59 +0100

    [Temps] Création des fichiers de gestion de Temps
    Nécessaire pour manipuler des dates. On va utiliser à 99% des fonctions de time.h mais j'avais besoin de créer quelques fonctions supplémentaires.

---
+++ src/Temps.c
@@ -0,0 +1,5 @@
+#include "Temps.h"
+
+// ---- CONSTANTES ---- //
+
+// ---- FONCTIONS ---- //
---
+++ src/Temps.h
@@ -0,0 +1,12 @@
+#ifndef DEF_Temps
+#define DEF_Temps
+
+// EN-TETE Temps.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+// ---- CONSTANTES ---- //
+
+// ---- FONCTIONS ---- //
