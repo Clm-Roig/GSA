7d8182f8d505480ce0220158572a2fefb097f741
Clément Roig <clm.roig@gmail.com>
2017-01-13 17:00:48 +0100

    [SDL_fenetres] Ajout de commentaires + texte
    Penser à checker le "TODO" et faire un retour à Clément !!

--- src/Sdl_fenetres.c
+++ src/Sdl_fenetres.c
@@ -1,27 +1,39 @@
 #include "Sdl_fenetres.h"
 
-void menu(){
+void menu() {
 	SDL_Surface* screenSurface;
 	SDL_Surface* texteAjout;
 	SDL_Surface* texteInv;
+
+	// Fond bleu
 	screenSurface=SDL_GetWindowSurface(getwindow());
 	SDL_FillRect(screenSurface,NULL,SDL_MapRGB(screenSurface->format,44, 62, 80));
 
-
-	SDL_Rect buttEnreg;SDL_Rect buttInv;
+	// Boutons menu
+	SDL_Rect buttEnreg; SDL_Rect buttInv;
 	buttEnreg.x=40; buttEnreg.y=40; buttEnreg.w=350; buttEnreg.h=400;
 	buttInv.x=400; buttInv.y=40; buttInv.w=350; buttInv.h=400;
+
 	SDL_FillRect(screenSurface,&buttEnreg,SDL_MapRGB(screenSurface->format,192, 57, 43));
 	SDL_FillRect(screenSurface,&buttInv,SDL_MapRGB(screenSurface->format,211, 84, 0));
+
+	// Texte boutons
 	SDL_Color couleurBlanc = {255, 255, 255};
-	texteAjout = TTF_RenderText_Blended(getpolice(), "Ajout", couleurBlanc);
+	texteAjout = TTF_RenderText_Blended(getpolice(), "Ajout d'un aliment", couleurBlanc);
 	texteInv = TTF_RenderText_Blended(getpolice(), "Mon stock", couleurBlanc);
-	
+
+	// Insertion du texte dans les boutons
+	// TODO : les boutons doivent être des SDL_Surface et non des SDL_Rect pour pouvoir
+	// y inscrire du texte dedans en exécutant SDL_BlitSurface()
+	if(!SDL_BlitSurface(texteAjout,NULL,screenSurface,NULL)) {
+		printf("Erreur Blit Surface  : %s\n", SDL_GetError());
+	}
+
 	SDL_UpdateWindowSurface(getwindow());
 }
 
 
-void peser(){
+void peser() {
 	SDL_Surface* screenSurface;
 	screenSurface=SDL_GetWindowSurface(getwindow());
 	SDL_FillRect(screenSurface,NULL,SDL_MapRGB(screenSurface->format,44, 62, 80));
@@ -31,7 +43,7 @@ void peser(){
 }
 
 
-void stock(){
+void stock() {
 	SDL_Surface* screenSurface;
 	screenSurface=SDL_GetWindowSurface(getwindow());
 	SDL_FillRect(screenSurface,NULL,SDL_MapRGB(screenSurface->format,44, 62, 80));
