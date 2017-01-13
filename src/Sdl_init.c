8c5fe33294b5a79b8909d9194ea22c74752b88bc
Clément Roig <clm.roig@gmail.com>
2017-01-13 17:05:56 +0100

    [SDL Init] Ajout de commentaires / nettoyage du code

--- src/Sdl_init.c
+++ src/Sdl_init.c
@@ -1,32 +1,35 @@
 #include "Sdl_init.h"
 
+// ---- Variables générales ---- //
 SDL_Window *screen;
 SDL_Renderer *renderer;
 TTF_Font *police;
 
-SDL_Renderer *getrenderer(){
+SDL_Renderer *getrenderer() {
     return renderer;
 }
-SDL_Window *getwindow(){
+SDL_Window *getwindow() {
     return screen;
 }
-TTF_Font *getpolice(){
+TTF_Font *getpolice() {
     return police;
 }
 
-void init(){
-	int screenwidth; int screenheight;
+// ---- Fonctions ---- //
+void init() {
+    int screenwidth; int screenheight;
 	screenwidth = SCREEN_WIDTH;
 	screenheight = SCREEN_HEIGHT;
 
-  screen = SDL_CreateWindow("Coal - Connected ",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,screenwidth, screenheight,SDL_WINDOW_OPENGL|SDL_WINDOW_BORDERLESS);
+    screen = SDL_CreateWindow("Coal - Connected ",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,screenwidth, screenheight,SDL_WINDOW_OPENGL|SDL_WINDOW_BORDERLESS);
 
-  renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_PRESENTVSYNC);
+    renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_PRESENTVSYNC);
 
-  if (screen == NULL || renderer == NULL) {
-      printf("Impossible d'initialiser le mode écran à : %s\n", SDL_GetError());
-      exit(1);
-  }
+    if (screen == NULL || renderer == NULL) {
+        printf("Impossible d'initialiser le mode écran à : %s\n", SDL_GetError());
+        exit(1);
+    }
+
 	if(TTF_Init() == -1) {
     	fprintf(stderr, "Erreur d'initialisation de TTF_Init : %s\n", TTF_GetError());
     	exit(EXIT_FAILURE);
@@ -35,13 +38,12 @@ void init(){
 	else {
         police=TTF_OpenFont("data/fonts/arial.ttf", 24);
         if(!police) {
-          printf("TTF_OpenFont : %s\n", TTF_GetError());
-		    }
-	}
-
+            printf("Erreur chargement TTF_OpenFont : %s\n", TTF_GetError());
+		}
+    }
 }
 
-void cleanup(){
+void cleanup() {
     SDL_DestroyRenderer(renderer);
     renderer = NULL;
     SDL_DestroyWindow(screen);
