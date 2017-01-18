afcdacb531ef1f715ff39ac60d549956b3a57688
Riviere Tristan <northriver@laposte.net>
2017-01-18 17:31:19 +0100

    &

--- src/Sdl_fenetres.c
+++ src/Sdl_fenetres.c
@@ -27,7 +27,7 @@ void menu() {
 	// TODO : les boutons doivent être des SDL_Surface et non des SDL_Rect pour pouvoir
 	// y inscrire du texte dedans en exécutant SDL_BlitSurface()
 	pos.x=40; pos.y=40;
-	SDL_BlitSurface(texteAjout,NULL,screenSurface,pos);
+	SDL_BlitSurface(texteAjout,NULL,screenSurface,&pos);
 	/*
 	pos.x=400; pos.y=400;
 	if(!SDL_BlitSurface(texteInv,NULL,screenSurface,pos)) {
