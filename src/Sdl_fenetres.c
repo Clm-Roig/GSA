6fa404087b8a2fe0002dc6b5400d6d73a31d9191
Riviere Tristan <northriver@laposte.net>
2017-01-21 17:04:14 +0100

    Debog

--- src/Sdl_fenetres.c
+++ src/Sdl_fenetres.c
@@ -738,7 +738,7 @@ int stock() {
 	duree3 = duree3 / 3600;
 	duree4 = duree4 / 3600;
 	duree5 = duree5 / 3600;
-
+	printf("%d %d %d %d\n",duree1, duree2, duree3, duree4 );
 	//Conversion en rapport à 2 semaines
 	//336h dans deux semaines
 	duree1 = rapport14J(duree1);
@@ -746,7 +746,7 @@ int stock() {
 	duree3 = rapport14J(duree3);
 	duree4 = rapport14J(duree4);
 	duree5 = rapport14J(duree5);
-
+	printf("%d %d %d %d\n",duree1, duree2, duree3, duree4 );	printf("%d %d %d %d\n",duree1, duree2, duree3, duree4 );
 	//Affichage barre duree peremption
 	SDL_Rect barre1;
 	SDL_Rect barre2;
@@ -759,7 +759,7 @@ int stock() {
 	barre4.x=(pos4.x)+40+pos4.w; barre4.y=pos4.y+15; barre4.w=(float)((float)1/(float)duree4)*600; barre4.h=30;
 	barre5.x=(pos5.x)+40+pos5.w; barre5.y=pos5.y+15; barre5.w=(float)((float)1/(float)duree5)*600; barre5.h=30;
 	
-	printf("%d %d %d %d\n",duree1, duree2, duree3, duree4 );
+
 	if(duree1<=30){
 		SDL_FillRect(screenSurface,&barre1,SDL_MapRGB(screenSurface->format,192, 57, 43));
 	}
