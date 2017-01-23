9e591eb4237518ccd4a3d14a3d583e7aa44a1319
Clément Roig <clm.roig@gmail.com>
2017-01-23 21:23:46 +0100

    typo

--- src/BDD.c
+++ src/BDD.c
@@ -303,7 +303,7 @@ int* getIdAlimentParCouleur(Couleur* coul, int precision) {
         float deltaB = cMaxBdd - cMinBdd;
 
         // Calcul H
-        if(cMaxBdd == rapRbdd) hBdd = 60.0* ( (rapGbdd - rapBbdd)/deltaB );
+        if(cMaxBdd == rapRbdd) hBdd = 60.0* ( (rapGbdd - rapBbdd) / deltaB );
         if(cMaxBdd == rapGbdd) hBdd = 60.0*( ( (rapBbdd - rapRbdd) / deltaB) + 2.0);
         if(cMaxBdd == rapBbdd) hBdd = 60.0*( ( (rapRbdd - rapGbdd) / deltaB) + 4.0);
 
