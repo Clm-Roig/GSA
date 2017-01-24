74db4c7b8dcd98934a4f874117b536472f730080
Clément Roig <clm.roig@gmail.com>
2017-01-24 17:29:24 +0100

    cpt pixe

--- src/ReconnaissanceV.c
+++ src/ReconnaissanceV.c
@@ -138,6 +138,8 @@ Couleur* couleurDominanteHorsFond(ImageBMP* image, Couleur* couleurFond) {
 
     }
 
+	printf("\n\n%d pixels composent l'aliment",cptPixelsAliment);
+
     // Calcul des moyennes pour chaque composante des pixels de l'aliment
     if(cptPixelsAliment != 0) {
         moyenneRAliment = moyenneRAliment / cptPixelsAliment;
