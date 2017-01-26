1bf85a728aa6f2e4959d7160c7d0c9afe4876422
Clément Roig <clm.roig@gmail.com>
2017-01-26 12:01:13 +0100

    test juste fonction

--- src/Sdl_main.c
+++ src/Sdl_main.c
@@ -1,7 +1,7 @@
 #include "Sdl_main.h"
 
 int main(int argc,char* args[]) {
-
+/*
 	int loop = 1;
 	int page = 1;
 
@@ -22,7 +22,7 @@ int main(int argc,char* args[]) {
 	}
 
 	cleanup();
-
+*/
 /*
 	if (1){
 		reinitPesees();
@@ -37,5 +37,12 @@ int main(int argc,char* args[]) {
 		supprimerDonneePesee(4);
 	}
 */
+
+	Couleur* coul = initCouleurAvecParam(185,50,20);
+	int* liste = getIdAlimentParCouleur(coul);
+	int i;
+	for(i=0; i<5 ;i++){
+		printf("\n%d",liste[i]);
+	}
 	return 0;
 }
